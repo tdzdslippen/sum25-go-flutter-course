44a447d192b6269c6dea55a30fec093d517d60fa
tdzdslippen <tdzdslippen@users.noreply.github.com>
2025-07-15 15:42:04 +0000

    deploy: 05aca7f1afdeb8e5af2b9a103597adec61780fe9

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,6 +11,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
 "assets/NOTICES": "82e69f205a036b62898ef6d3b05a3cd5",
 "version.json": "1bac121e821a68e8eb01ab127700946e",
+"flutter_bootstrap.js": "4208f9c207ec5a1da83571c3990e8089",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
 "canvaskit/canvaskit.wasm": "3d2a2d663e8c5111ac61a46367f751ac",
